4a03c9418258d1653ddf80dd28f4ea3f1f856865
Nathan Dwek <n.dwek@hotmail.com>
2013-10-14 17:27:51 +0200

    added exercice 4_2

---
+++ dotMs/anQR.m
@@ -0,0 +1,16 @@
+function [R]=anQR(A)
+   R=A;
+   n=min(size(A));
+   m=max(size(A));
+   if n==m;
+      n=n-1;
+   end
+   vs=[];
+   for k=1:n;
+      x=R(k:m,k);
+      v=x;
+      v(1)=x(1)+sign(x(1))*norm(x);
+      v=v/norm(v);
+      R(k:m,k:n)=R(k:m,k:n)-2*(v*(v'*R(k:m,k:n)));
+   end
+end
