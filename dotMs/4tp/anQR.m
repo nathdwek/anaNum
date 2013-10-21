b52bdaee03ac2d4f4ea931e2b1e5501dba45e328
nathdwek <nathdwek@ulb.ac.be>
2013-10-21 22:34:06 +0200

    FINISHED TP 4 QR WITH Q FOR EVERY MXN M>N MATRIXgit add ../../pdfs/an_corrige4.pdfgit add ../../pdfs/an_corrige4.pdf

--- dotMs/4tp/anQR.m
+++ dotMs/4tp/anQR.m
@@ -2,17 +2,18 @@ function [Q R]=anQR(A)
    R=A;
    n=min(size(A));
    m=max(size(A));
-   Q=eye(n);
+   Q=eye(m);
    V=zeros(n);
    for k=1:n;
       x=R(k:m,k);
-      v=x;
-      v(1)=x(1)+sign(x(1))*norm(x);
-      v=v/norm(v);
+      x(1)=x(1)+sign(x(1))*norm(x);
+      v=x/norm(x);
       V(k:m,k)=v;
       R(k:m,k:n)=R(k:m,k:n)-2*(v*(v'*R(k:m,k:n)));
    end
-   for k=1:n
-      Q(n-k:n,n-k:n)=(eye(k+1)-2*V(n-k:m,n-k)*(V(n-k:m,n-k))')*Q(n-k:n,n-k:n); %A voir
+   for k=0:n-1
+      v=V(n-k:m,n-k);
+      w=2*v*v';
+      Q(n-k:m,n-k:m)=Q(n-k:m,n-k:m)-w*Q(n-k:m,n-k:m);
    end
 end
