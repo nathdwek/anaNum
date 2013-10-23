59dde83e199ef98aaea567cd5b193f99463ae878
nathdwekHome <nathdwek@ulb.ac.be>
2013-10-23 22:25:55 +0200

    started PLU function, struggling with arraylist and matrixes

--- dotMs/3tp/anPLU.m
+++ dotMs/3tp/anPLU.m
@@ -1,18 +1,34 @@
-function [L U] = anLu(A)
+function [L U P] = anPLU(A)
+   U=A;
    n = length(A);
-   for k = 1:n
-      for j = k:n
-         U(k,j) = A(k,j); %garde la "première" ligne dans son entièreté. "Je ne touche plus à ça"
-      end
-      L(k,k) = 1; %raccourci de l'instruction ligne 9, sans calculer A(k,k)/A(k,k). "La première ligne, je fais fois 1"
-      for i = k+1:n
-         L(i,k) = A(i,k)/A(k,k); %les autres coefficients qui multiplient les lignes (cf élimination de Gauss)
-      end
-      for i = k+1:n
-         for j = k+1:n
-         A(i,j) = A(i,j)-L(i,k)*U(k,j); %j'applique L sur A. "je travaille sur A et stocke (ligne 5) les lignes propres/définitives dans U"
-         end
-      end
+   pList={};
+   lList={};
+   lpList={};
+   for k = 1:n-1
+      P=eye(n);
+      maxIndex=indexMaxAbs(U(:,k));
+      P([1,maxIndex],:)=P([maxIndex,1],:);
+      U=P*U;
+      L=eye(n);
+      L(k+1:n,k)=-U(k+1:n,k)./U(k,k);
+      U=L*U;
+      pList(k)=P;
+      lList(k)=L;
+   end
+   thePright=eye(n);
+   thePleft=eye(n);
+   for k=n-1:-1:2
+      lpList(k)=thePleft*lList(k)*thePright;
+      thePright=pList(k)*thePright;
+      thePleft=thePleft*pList(k);
+   end
+   P=thePleft*P1;
+   L=eye(n)
+   for k=n:-1:2
+      L(k:n,k-1)=-lpList(k-1)(k:n,k-1)
    end
 end
+
+
+
 
