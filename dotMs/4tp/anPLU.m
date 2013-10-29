6b5a43af65697bc007a297b7268c40f196aa770f
nathdwekULB <nathdwek@ulb.ac.be>
2013-10-29 11:50:59 +0100

    enhanced algorithm, fully commented anPLU

--- dotMs/4tp/anPLU.m
+++ dotMs/4tp/anPLU.m
@@ -1,34 +1,39 @@
 function [L U P] = anPLU(A)
+
+   %initialisation
    U=A;
    n = length(A);
-   pList={};
-   lList={};
-   lpList={};
+   pList={}; %liste des matrices de permutation des étapes successives
+   lList={}; %liste des matrices d'élimination des étapes successives
+   lpList={}; %liste des matrices de type P3*P2*L1*P2*P3
+
+   %Algo de permutation-élimination. But:Produire une liste de matrices de permutation et d'élimination, ainsi que U
    for k = 1:n-1
-      P=eye(n);
-      maxIndex=indexMaxAbs(U(:,k));
-      P([k,maxIndex],:)=P([maxIndex,k],:);
+      P=eye(n); %nouvelle matrice de permutation identité
+      maxIndex=indexMaxAbs(U(:,k)); %trouver la position de l'élément le plus grand en valeur absolue dans la colonne considérée
+      P([k,maxIndex],:)=P([maxIndex,k],:); %former la matrice de permuation pour amener cet élément sur la diagonale
       U=P*U;
-      L=eye(n);
-      L(k+1:n,k)=-U(k+1:n,k)./U(k,k);
+      L=eye(n); %nouvelle matrice d'élimination
+      L(k+1:n,k)=-U(k+1:n,k)./U(k,k); %Gauss
       U=L*U;
       pList(k)=P;
       lList(k)=L;
    end
 
-   thePright=eye(n);
-   thePleft=eye(n);
-   for k=n-1:-1:2
-      lpList(k)=thePleft*lList{k}*thePright;
-      thePright=pList{k}*thePright;
-      thePleft=thePleft*pList{k};
+   %formation de P et d'une liste de Lprims; à partir de la liste des P des L successifs
+   thePright=pList{end}; %le produit de matrices qui va multiplier à droite
+   thePleft=pList{end}; %le produit de matrices qui va multiplier à droite
+   lpList(n-1)=lList(end); %la première Lprim formée (correspondant à la dernière matrice d'élimination L) est égale à cette matrice L lui correspondant (ex:n=4: Lp3=L3)
+   for k=n-2:-1:1 %cf pdf
+      lpList(k)=thePleft*lList{k}*thePright; %ex: Lp2=P4*P3*L2*P3*P4
+      thePright=pList{k}*thePright; %ex: Pdroite=P2*Pdroite=P2*P3*P4
+      thePleft=thePleft*pList{k}; %ex: Pgauche=Pgauche*P2=P4*P3*P2
    end
-   lpList(1)=thePleft*lList{1}*thePright;
-   P=thePleft*pList{1};
-   L=eye(n);
-   lpList;
-   lList;
+   P=thePleft;
+
+   %formation de L à partir des Lprims successifs
+   L=eye(n); %Matrice L "finale" /!\ formée nottament à partir des L dans l'algo plus haut, mais pas égale à celles-ci!
    for k=n:-1:2
-      L(k:n,k-1)=-lpList{k-1}(k:n,k-1);
+      L(k:n,k-1)=-lpList{k-1}(k:n,k-1); %cf pdf/cours
    end
 end
